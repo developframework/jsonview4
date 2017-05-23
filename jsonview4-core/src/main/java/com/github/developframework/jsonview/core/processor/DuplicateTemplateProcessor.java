570ac83057cd11d5d555750ccbbc4191bfe61780
developframework <408000511@qq.com>
2017-05-23 11:11:59 +0800

    修复在<array>中使用<extend-port>出现的bug

--- jsonview4-core/src/main/java/com/github/developframework/jsonview/core/processor/DuplicateTemplateProcessor.java
+++ jsonview4-core/src/main/java/com/github/developframework/jsonview/core/processor/DuplicateTemplateProcessor.java
@@ -6,6 +6,8 @@ import com.github.developframework.expression.Expression;
 import com.github.developframework.jsonview.core.element.Element;
 import com.github.developframework.jsonview.core.element.ObjectElement;
 
+import java.util.Optional;
+
 /**
  * 副本模板节点处理器
  * @author qiuzhenhao
@@ -25,7 +27,21 @@ public class DuplicateTemplateProcessor extends ObjectProcessor{
     @Override
     public void handleCoreLogic(ContentProcessor<? extends Element, ? extends JsonNode> parentProcessor) {
         if (parentProcessor instanceof ObjectInArrayProcessor) {
-            ObjectInArrayProcessor processor = new ObjectInArrayProcessor(processContext, element, parentProcessor.expression, ((ObjectInArrayProcessor) parentProcessor).size);
+            ObjectInArrayProcessor processor = new ObjectInArrayProcessor(processContext, element, parentProcessor.expression, ((ObjectInArrayProcessor) parentProcessor).size) {
+
+                @Override
+                protected boolean prepare(ContentProcessor<? extends Element, ? extends JsonNode> parentProcessor) {
+                    Optional<Object> valueOptional = processContext.getDataModel().getData(expression);
+                    if (valueOptional.isPresent()) {
+                        this.value = valueOptional.get();
+                        // this.node = processContext.getJsonviewConfiguration().getObjectMapper().createObjectNode();
+                        // 需要重写这个地方 不再传入新建的ObjectNode，而是使用父处理器的node
+                        this.node = (ObjectNode) parentProcessor.node;
+                        return true;
+                    }
+                    return false;
+                }
+            };
             processor.setNode((ObjectNode) parentProcessor.node);
             processor.process(parentProcessor);
         } else {
